7ca467a3e00a4188917ba4bed022d5fe116237cf
osparking <jbpark03@gmail.com>
2024-06-07 09:47:13 +0900

    타코 구름 응용 - 종속성 3개

---
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -0,0 +1,14 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudApplication.class, args);
+	}
+
+}

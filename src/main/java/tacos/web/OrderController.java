5ac54c440dbfb085b3bb8e6469630da1baad96bd
osparking <jbpark03@gmail.com>
2024-06-09 21:51:44 +0900

    타코 주문 POST 요청 처리 메소드

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -2,10 +2,13 @@ package tacos.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import lombok.extern.slf4j.Slf4j;
+import tacos.TacoOrder;
 
 @Slf4j
 @Controller
@@ -18,4 +21,11 @@ public class OrderController {
     return "orderForm";
   }
   
+  @PostMapping
+  public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+    log.info("타코 주문 : {}", order);
+    sessionStatus.setComplete();
+    
+    return "redirect:/";
+  }
 }

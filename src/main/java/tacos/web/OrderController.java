ba3f840f4f4f911b8e489d7b7bd4e8eb32f0792c
osparking <jbpark03@gmail.com>
2024-06-11 14:48:40 +0900

    타코 주문 내역 검증 필요성 명세

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -1,12 +1,14 @@
 package tacos.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import tacos.TacoOrder;
 
@@ -20,12 +22,18 @@ public class OrderController {
   public String processOrder() {
     return "orderForm";
   }
-  
+
   @PostMapping
-  public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+  public String processOrder(@Valid TacoOrder order, Errors errors,
+      SessionStatus sessionStatus) {
+
+    if (errors.hasErrors()) {
+      return "orderForm";
+    }
+
     log.info("타코 주문 : {}", order);
     sessionStatus.setComplete();
-    
+
     return "redirect:/";
   }
 }

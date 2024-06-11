ebdb28bf595224f7d89d13a0f8d22f9cac269bcc
osparking <jbpark03@gmail.com>
2024-06-11 09:13:01 +0900

    이름 및 재료 검증 애노'

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -2,10 +2,17 @@ package tacos;
 
 import java.util.List;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class Taco {
+  @NotNull
+  @Size(min = 2, message = "이름은 최소 2 문자라야 됩니다.")
   private String name;
+
+  @NotNull
+  @Size(min = 1, message = "재료를 한 가지 이상 선택하세요.")
   private List<Ingredient> ingredients;
 }

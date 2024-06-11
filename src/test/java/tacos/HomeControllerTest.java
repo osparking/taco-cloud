4ce09d7a3d2f53ad558258404c5afcaa66d6cb1f
osparking <jbpark03@gmail.com>
2024-06-11 21:40:51 +0900

    불필요한 Home 제어기 삭제 등

--- src/test/java/tacos/HomeControllerTest.java
+++ src/test/java/tacos/HomeControllerTest.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(HomeController.class)
 class HomeControllerTest {
 
   @Autowired

5386436c885a1a0b38de631a2c63949526425f5c
osparking <jbpark03@gmail.com>
2024-06-14 14:13:27 +0900

    개선된 제어기에 맞춰 개선된 저장소

--- src/main/java/tacos/repository/IngredientRepository.java
+++ src/main/java/tacos/repository/IngredientRepository.java
@@ -23,44 +23,25 @@ public class IngredientRepository {
   DataSource dataSource;
 
   public Optional<Ingredient> findById(String id) {
-    Connection connection = null;
-    PreparedStatement statement = null;
-    ResultSet resultSet = null;
 
-    try {
-      connection = dataSource.getConnection();
-      statement = connection
-          .prepareStatement("select id, name, type from Ingredient where id=?");
-      statement.setString(1, id);
-      resultSet = statement.executeQuery();
-      Ingredient ingredient = null;
-      if (resultSet.next()) {
-        ingredient = new Ingredient(resultSet.getString("id"),
-            resultSet.getString("name"),
-            Type.values()[resultSet.getInt("type")]);
+    String findIngID = "select id, name, type from Ingredient where id=?";
+
+    try (Connection connection = dataSource.getConnection();
+        PreparedStatement statement = connection
+            .prepareStatement(findIngID);) {
+
+      statement.setString(1, id.toUpperCase());
+      try (ResultSet resultSet = statement.executeQuery()) {
+        if (resultSet.next()) {
+          return Optional.of(new Ingredient(resultSet.getString("id"),
+              resultSet.getString("name"),
+              Type.values()[resultSet.getInt("type")]));
+        }
+      } catch (Exception e) {
+        log.error(e.getMessage());
       }
-      return Optional.of(ingredient);
     } catch (SQLException e) {
       log.error(e.getMessage());
-    } finally {
-      if (resultSet != null) {
-        try {
-          resultSet.close();
-        } catch (SQLException e) {
-        }
-      }
-      if (statement != null) {
-        try {
-          statement.close();
-        } catch (SQLException e) {
-        }
-      }
-      if (connection != null) {
-        try {
-          connection.close();
-        } catch (SQLException e) {
-        }
-      }
     }
     return Optional.empty();
   }

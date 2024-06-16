81d98113af6add3fe4e55abb7e42a4ff0c938924
osparking <jbpark03@gmail.com>
2024-06-16 09:34:46 +0900

    제어기 클래스 수정

--- src/main/java/tacos/controller/IngredientController.java
+++ src/main/java/tacos/controller/IngredientController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
-import tacos.repository.IngredientRepository;
+import tacos.data.IngredientRepository;
 
 @RestController
 @Slf4j

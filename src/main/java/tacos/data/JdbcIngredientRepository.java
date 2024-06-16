4463aaf784792ed075a6dd8038079624c69bf76f
osparking <jbpark03@gmail.com>
2024-06-16 09:25:06 +0900

    읽은 DB 테이블 행을 재료 객체화하는 메소드

--- src/main/java/tacos/data/JdbcIngredientRepository.java
+++ src/main/java/tacos/data/JdbcIngredientRepository.java
@@ -1,22 +1,32 @@
 package tacos.data;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Optional;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import tacos.Ingredient;
+import tacos.Ingredient.Type;
 
 @Repository
 public class JdbcIngredientRepository implements IngredientRepository {
 
   private JdbcTemplate jdbcTemplate;
-  
+
   public JdbcIngredientRepository(JdbcTemplate jdbcTemplate) {
     super();
     this.jdbcTemplate = jdbcTemplate;
   }
-  
+
+  private Ingredient mapRowToIngredient(ResultSet row, int rowNum)
+      throws SQLException {
+    return new Ingredient(
+        row.getString("id"),
+        row.getString("name"),
+        Type.values()[row.getInt("type")]);
+  }
 
   @Override
   public Iterable<Ingredient> findAll() {

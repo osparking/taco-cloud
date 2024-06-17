55c2dde929666a98bef8a3b00397857785fbde51
osparking <jbpark03@gmail.com>
2024-06-17 14:58:31 +0900

    재료 저장소 클래스 save 메소드 활용

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -1,8 +1,7 @@
 package tacos.web;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,12 +18,19 @@ import tacos.Ingredient;
 import tacos.Ingredient.Type;
 import tacos.Taco;
 import tacos.TacoOrder;
+import tacos.data.IngredientRepository;
 
 @Slf4j
 @Controller
 @RequestMapping("/design")
 @SessionAttributes("tacoOrder")
 public class DesignTacoController {
+  private final IngredientRepository ingredientRepo;
+
+  public DesignTacoController(
+      IngredientRepository ingredientRepo) {
+    this.ingredientRepo = ingredientRepo;
+  }
 
   @PostMapping
   public String processTaco(@Valid Taco taco, Errors errors,
@@ -41,26 +47,21 @@ public class DesignTacoController {
   }
 
   private Iterable<Ingredient> ingredientByType(
-      List<Ingredient> ingredients, Type type) {
-    return ingredients
-        .stream()
-        .filter(x -> x.getType().equals(type))
-        .collect(Collectors.toList());
+      Iterable<Ingredient> ingredients, Type type) {
+    var ings = ingredients.iterator();
+    List<Ingredient> ingOfType = new ArrayList<>();
+    while (ings.hasNext()) {
+      var x = ings.next();
+      if (x.getType().equals(type)) {
+        ingOfType.add(x);
+      }
+    }
+    return ingOfType;
   }
 
   @ModelAttribute
   public void addIngredientsToModel(Model model) {
-    List<Ingredient> ingredients = Arrays.asList(
-        new Ingredient("FLTO", "밀가루 토르티야", Type.WRAP),
-        new Ingredient("COTO", "옥수수 토르티야", Type.WRAP),
-        new Ingredient("GRBF", "다진 소고기", Type.PROTEIN),
-        new Ingredient("CARN", "삶은 돼지고기", Type.PROTEIN),
-        new Ingredient("TMTO", "토마토 깍뚜기", Type.VEGGIES),
-        new Ingredient("LETC", "상추", Type.VEGGIES),
-        new Ingredient("CHED", "체더치즈", Type.CHEESE),
-        new Ingredient("JACK", "몬테레이 잭", Type.CHEESE),
-        new Ingredient("SLSA", "살사", Type.SAUCE),
-        new Ingredient("SRCR", "사워 크림", Type.SAUCE));
+    Iterable<Ingredient> ingredients = ingredientRepo.findAll();
 
     Type[] types = Ingredient.Type.values();
     for (Type type : types) {

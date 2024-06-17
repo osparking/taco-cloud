abdb2093204d6eccaa30184f25c9236aec0062be
osparking <jbpark03@gmail.com>
2024-06-17 11:48:27 +0900

    수정 가능한 Ingredient 화

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -1,12 +1,16 @@
 package tacos;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Ingredient {
-  private final String id;
-  private final String name;
-  private final Type type;
+  private String id;
+  private String name;
+  private Type type;
   
   public enum Type {
     WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE

0e20865f8331efadc1a2a20b8104be0bc351e13a
osparking <jbpark03@gmail.com>
2024-06-17 11:54:07 +0900

    새 재료 생성용 모델 및 2 처리 메소드

--- src/main/java/tacos/controller/IngredientController.java
+++ src/main/java/tacos/controller/IngredientController.java
@@ -1,17 +1,20 @@
 package tacos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
+import tacos.Ingredient;
 import tacos.data.IngredientRepository;
 
-@RestController
 @Slf4j
 @RequestMapping
+@Controller
 public class IngredientController {
 
   @Autowired
@@ -26,5 +29,23 @@ public class IngredientController {
     } else {
       log.error(idUpper + ": 재료가 없습니다");
     }
+  }
+
+  @ModelAttribute(name = "ingredient")
+  public Ingredient ingredient() {
+    return new Ingredient();
+  }
+
+  @GetMapping("/ingredient/new")
+  public String newIngredientForm() {
+    return "ingredientForm";
+  }
+
+  @PostMapping("/ingredient/new")
+  public String saveIngredient(Ingredient ingredient) {
+    Number newSN = repository.save(ingredient);
+    log.info("추가된 재료 일련번호: {}", newSN);
+
+    return "redirect:/";
   }
 }

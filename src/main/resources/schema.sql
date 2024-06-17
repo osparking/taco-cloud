86f99c0f99959b2c3b1116a6eb520a4b77fe9179
osparking <jbpark03@gmail.com>
2024-06-18 08:03:13 +0900

    테이블 정의 3 건 스키마에 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,3 +9,34 @@ CREATE CACHED TABLE "PUBLIC"."INGREDIENT"(
 ALTER TABLE "PUBLIC"."INGREDIENT" ADD CONSTRAINT "PUBLIC"."ING_PRI_KEY" PRIMARY KEY("SN");
 
 ALTER TABLE "PUBLIC"."INGREDIENT" ADD CONSTRAINT ID_UNIQUE UNIQUE(ID);
+
+create table if not exists Taco_Order (
+  id identity,
+  cust_name varchar(50) not null,
+  deli_zip varchar(10) not null,
+  deli_road_addr varchar(50) not null,
+  deli_detail_addr varchar(50) not null,
+  cc_number varchar(16) not null,
+  cc_cvv varchar(3) not null,
+  cc_expr_y_m varchar(5) not null,
+  placed_at timestamp not null
+);
+ 
+create table if not exists Taco (
+  id identity,
+  name varchar(50) not null,
+  order_id bigint not null,
+  order_key bigint not null,
+  created_at timestamp not null
+);
+ 
+create table if not exists Ingredient_Ref (
+  ing_sn varchar(4) not null,
+  taco_id bigint not null,
+  taco_key bigint not null
+);
+ 
+alter table Taco
+    add foreign key (order_id) references Taco_Order(id);
+alter table Ingredient_Ref
+    add foreign key (ing_sn) references Ingredient(sn);

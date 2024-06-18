01fe03e550579e70b8f2dd60ec5f801e97197a78
osparking <jbpark03@gmail.com>
2024-06-18 20:34:59 +0900

    CrudRepo 확장 - 가장 짧은 코드

--- src/main/java/tacos/data/IngredientRepository.java
+++ src/main/java/tacos/data/IngredientRepository.java
@@ -1,16 +1,9 @@
 package tacos.data;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.CrudRepository;
 
 import tacos.Ingredient;
 
-public interface IngredientRepository extends Repository<Ingredient, Short> {
-  List<Ingredient> findAll();
-
-  Optional<Ingredient> findById(String id);
-
-  Ingredient save(Ingredient ingredient);
+public interface IngredientRepository
+    extends CrudRepository<Ingredient, Short> {
 }

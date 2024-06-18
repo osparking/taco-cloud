29297669a78a9857d8cd6d4e2a5f5289374e7aa8
osparking <jbpark03@gmail.com>
2024-06-18 20:34:59 +0900

    CrudRepo 확장 - JPA, JDBC 둘다 지원됨

--- src/main/java/tacos/data/IngredientRepository.java
+++ src/main/java/tacos/data/IngredientRepository.java
@@ -1,14 +1,9 @@
 package tacos.data;
 
-import java.util.List;
-import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
 
 import tacos.Ingredient;
 
-public interface IngredientRepository {
-  List<Ingredient> findAll();
-
-  Optional<Ingredient> findById(String id);
-
-  Number save(Ingredient ingredient);
+public interface IngredientRepository
+    extends CrudRepository<Ingredient, Short> {
 }

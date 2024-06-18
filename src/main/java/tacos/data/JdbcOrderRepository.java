2fe14c4b15fe936341a97726c385449482aa94b6
osparking <jbpark03@gmail.com>
2024-06-18 12:14:58 +0900

    JdbcTemplate 을 사용한 타코 주문 저장소

---
+++ src/main/java/tacos/data/JdbcOrderRepository.java
@@ -0,0 +1,69 @@
+package tacos.data;
+
+import java.sql.Types;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.PreparedStatementCreatorFactory;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+
+import tacos.Taco;
+import tacos.TacoOrder;
+
+public class JdbcOrderRepository implements OrderRepository {
+
+  private JdbcOperations jdbcOperations;
+
+  public JdbcOrderRepository(JdbcOperations jdbcOperations) {
+    this.jdbcOperations = jdbcOperations;
+  }
+
+  @Override
+  public TacoOrder save(TacoOrder order) {
+    StringBuilder insertStmt = new StringBuilder();
+    insertStmt.append("insert into Taco_Order ");
+    insertStmt.append("(cust_name, deli_zip, deli_road_addr, ");
+    insertStmt.append("deli_detail_addr, cc_number, cc_cvv, ");
+    insertStmt.append("cc_expr_y_m, placed_at) ");
+    insertStmt.append("values (?,?,?,?,?,?,?,?)");
+
+    PreparedStatementCreatorFactory pscf = new PreparedStatementCreatorFactory(
+        insertStmt.toString(),
+        Types.VARCHAR, Types.VARCHAR, Types.VARCHAR,
+        Types.VARCHAR, Types.VARCHAR, Types.VARCHAR,
+        Types.VARCHAR, Types.TIMESTAMP);
+    pscf.setReturnGeneratedKeys(true);
+
+    order.setPlacedAt(LocalDateTime.now());
+    PreparedStatementCreator psc = pscf.newPreparedStatementCreator(
+        Arrays.asList(
+            order.getCustName(),
+            order.getDeliZip(),
+            order.getDeliRoadAddr(),
+            order.getDeliDetailAddr(),
+            order.getCcNumber(),
+            order.getCcCvv(),
+            order.getCcExprYM(),
+            order.getPlacedAt()));
+
+    GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
+    jdbcOperations.update(psc, keyHolder);
+    long orderId = keyHolder.getKey().longValue();
+    order.setId(orderId);
+
+    List<Taco> tacos = order.getTacos();
+    int i = 0;
+    for (Taco taco : tacos) {
+      saveTaco(orderId, i++, taco);
+    }
+
+    return order;
+  }
+
+  private void saveTaco(long orderId, int i, Taco taco) {
+    // TODO Auto-generated method stub
+  }
+}

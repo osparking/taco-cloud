4147cc5dd1e38a515eb5bbdad835ef81baf07d03
osparking <jbpark03@gmail.com>
2024-06-19 13:47:27 +0900

    Table - 선택적 애노테이션

--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.hibernate.validator.constraints.CreditCardNumber;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
@@ -13,6 +14,7 @@ import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Table
 public class TacoOrder {
 
   @Id

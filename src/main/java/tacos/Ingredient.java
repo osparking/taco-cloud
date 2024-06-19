12a0c597b128ab91ec0b4a267f84e3d75289471d
osparking <jbpark03@gmail.com>
2024-06-19 18:20:50 +0900

    Runner 인터 사용 재료 초기화 지원

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -1,18 +1,30 @@
 package tacos;
 
+
 import org.springframework.data.annotation.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Ingredient {
   @Id
-  private final short sn;
-  private final String id;
-  private final String name;
-  private final Type type;
+  private Short sn;
+  private String id;
+  private String name;
+  private Short typeOrd;
   
   public enum Type {
     WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+  }
+
+  public Ingredient(String id, String name, short typeOrd) {
+    super();
+    this.id = id;
+    this.name = name;
+    this.typeOrd = typeOrd;
   }
 }

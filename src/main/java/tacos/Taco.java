91f626ae9be64fa1b1fa282a70b38cb323b1e1c2
osparking <jbpark03@gmail.com>
2024-06-20 17:04:29 +0900

    JPA 애노' 지정- Taco 타코

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,15 +1,25 @@
 package tacos;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
+@Entity
 public class Taco {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
   private LocalDateTime createdAt = LocalDateTime.now();
@@ -20,5 +30,10 @@ public class Taco {
 
   @NotNull
   @Size(min = 1, message = "재료를 한 가지 이상 선택하세요.")
-  private List<Ingredient> ingredients;
+  @ManyToAny
+  private List<Ingredient> ingredients = new ArrayList<>();
+
+  public void addIngredient(Ingredient ingredient) {
+    this.ingredients.add(ingredient);
+  }
 }

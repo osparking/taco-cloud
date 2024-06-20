06a1298ed7165d37a90e16e2fc9bf6a2f727ddd7
osparking <jbpark03@gmail.com>
2024-06-21 07:18:55 +0900

    우편번호로 타코 주문 목록 검색

--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -1,7 +1,11 @@
 package tacos.data;
 
+import java.util.List;
+
 import tacos.TacoOrder;
 
 public interface OrderRepository {
   TacoOrder save(TacoOrder order);
+
+  List<TacoOrder> findByDeliZip(String deliZip);
 }

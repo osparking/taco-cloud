d74da70d6476adec1b59d4470610cb63adb99d42
osparking <jbpark03@gmail.com>
2024-06-21 11:44:57 +0900

    type 을 typeOrd로, 생성자 추가

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -14,12 +14,19 @@ import lombok.NoArgsConstructor;
 public class Ingredient {
 
   @Id
-  private final short sn;
+  private short sn;
   private final String id;
   private final String name;
-  private final Type type;
+  private final Short typeOrd;
   
   public enum Type {
     WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+  }
+
+  public Ingredient(String id, String name, short typeOrd) {
+    super();
+    this.id = id;
+    this.name = name;
+    this.typeOrd = typeOrd;
   }
 }

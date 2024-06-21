f46c8fc8e3ff500e771c5795c865d6922e2191c3
osparking <jbpark03@gmail.com>
2024-06-21 17:22:23 +0900

    재료참조 테이블 저장 대상, id 멤버 필요

--- src/main/java/tacos/IngredientRef.java
+++ src/main/java/tacos/IngredientRef.java
@@ -1,10 +1,17 @@
 package tacos;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class IngredientRef {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
   private final short ingredientSN; // 재료 일련번호
-
 }

fd4be74c293b6c45b694443b19412437a963f446
osparking <jbpark03@gmail.com>
2024-06-21 11:22:55 +0900

    SD JDBC 에서 작동하는 자료sql로...

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,5 @@
-INSERT INTO INGREDIENT(ID, NAME, TYPE) VALUES ('FLTO', '밀가루 토르티야', 0);
-INSERT INTO INGREDIENT(ID, NAME, TYPE) VALUES ('COTO', '옥수수 토르티야', 0);   
-INSERT INTO INGREDIENT(ID, NAME, TYPE) VALUES ('GRBF', '다진 소고기', 1);  
-INSERT INTO INGREDIENT(ID, NAME, TYPE) VALUES ('CARN', '삶은 돼지고기', 1); 
-INSERT INTO INGREDIENT(ID, NAME, TYPE) VALUES ('TMTO', '토마토 깍뚜기', 2); 
-INSERT INTO INGREDIENT(ID, NAME, TYPE) VALUES ('LETC', '상추', 2);      
-INSERT INTO INGREDIENT(ID, NAME, TYPE) VALUES ('CHED', '체더치즈', 3);     
-INSERT INTO INGREDIENT(ID, NAME, TYPE) VALUES ('JACK', '몬테레이 잭', 3);   
-INSERT INTO INGREDIENT(ID, NAME, TYPE) VALUES ('SLSA', '살사', 4);        
-INSERT INTO INGREDIENT(ID, NAME, TYPE) VALUES ('SRCR', '사워 크림', 4);        
-        
+delete from Ingredient_Ref;
+delete from Taco;
+delete from Taco_Order;
+
+delete from Ingredient;

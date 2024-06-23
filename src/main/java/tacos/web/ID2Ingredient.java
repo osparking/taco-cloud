efdba65601466b87ec1c85283bdfe3f7ce07f0db
osparking <jbpark03@gmail.com>
2024-06-23 16:25:08 +0900

    재료 ID(long)를 재료로 바꾸는 맴 활용 효율화

--- src/main/java/tacos/web/ID2Ingredient.java
+++ src/main/java/tacos/web/ID2Ingredient.java
@@ -1,5 +1,10 @@
 package tacos.web;
 
+import static java.lang.Short.valueOf;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
@@ -8,14 +13,30 @@ import tacos.data.IngredientRepository;
 
 @Component
 public class ID2Ingredient implements Converter<String, Ingredient> {
+
   private final IngredientRepository ingredientRepo;
 
   public ID2Ingredient(IngredientRepository repository) {
     ingredientRepo = repository;
   }
 
+  private Map<Short, Ingredient> ingredientMap = new HashMap<>();
+
+  private void makeMap() {
+    try {
+      ingredientRepo.findAll().forEach(ing -> {
+        ingredientMap.put(Short.valueOf(ing.getId().toString()), ing);
+      });
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
   @Override
   public Ingredient convert(String sn) {
-    return ingredientRepo.findById(Short.valueOf(sn)).orElse(null);
+    if (ingredientMap.size() == 0) {
+      makeMap();
+    }
+    return ingredientMap.get(valueOf(sn));
   }
 }

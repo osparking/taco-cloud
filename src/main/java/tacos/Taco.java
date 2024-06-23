fa765180d124dcdd6668f47c28e2fb14492e9ff3
osparking <jbpark03@gmail.com>
2024-06-23 14:18:03 +0900

    다대다 관계 기초 작업 - 타코 측

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -4,11 +4,12 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -28,9 +29,13 @@ public class Taco {
   @Size(min = 2, message = "이름은 최소 2 문자라야 됩니다.")
   private String name;
 
+  // @formatter:off
   @NotNull
   @Size(min = 1, message = "재료를 한 가지 이상 선택하세요.")
-  @ManyToMany(cascade = CascadeType.ALL)
-  private List<IngredientRef> ingredientRefs = new ArrayList<>();
-
+  @ManyToMany
+  @JoinTable(name = "taco_ingredients", 
+    joinColumns = @JoinColumn(name = "taco_id"), 
+    inverseJoinColumns = @JoinColumn(name = "ingredient_id"))
+  private List<Ingredient> includedIngredients = new ArrayList<>();
+  // @formatter:on
 }

8988fc1b417b4f96d1d64a880e35029bd1b92601
osparking <jbpark03@gmail.com>
2024-07-22 20:31:03 +0900

    비소유측(TacoOrder) 멤버인 tacos 에 mappedBy 성질값 지정

--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -8,6 +8,7 @@ import org.hibernate.validator.constraints.CreditCardNumber;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -27,30 +28,30 @@ public class TacoOrder {
 
   private LocalDateTime placedAt = LocalDateTime.now();
 
-  @NotBlank(message="고객 성명은 필수 입력 항목입니다.")
+  @NotBlank(message = "고객 성명은 필수 입력 항목입니다.")
   private String custName; // 고객명
-  
-  @OneToMany(cascade = CascadeType.ALL)
+
+  @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, 
+      mappedBy = "order")
   private List<Taco> tacos = new ArrayList<>();
-  
-  @NotBlank(message="우편번호는 필수 입력 항목입니다.")
+
+  @NotBlank(message = "우편번호는 필수 입력 항목입니다.")
   private String deliZip; // 우편번호
-  
-  @NotBlank(message="도로 주소는 필수 입력 항목입니다.")
+
+  @NotBlank(message = "도로 주소는 필수 입력 항목입니다.")
   private String deliRoadAddr; // 도로 주소
 
-  @NotBlank(message="상세 주소는 필수 입력 항목입니다.")
+  @NotBlank(message = "상세 주소는 필수 입력 항목입니다.")
   private String deliDetailAddr; // 상세 주소
-  
-  @CreditCardNumber(message="신용카드 번호가 정당하지 않습니다.")
+
+  @CreditCardNumber(message = "신용카드 번호가 정당하지 않습니다.")
   private String ccNumber;
-  
-  @NotBlank(message=" 필수 입력 항목입니다.")
-  @Digits(integer=3, fraction=0, message="CVV 입력 형식이 잘못되었습니다.")
+
+  @NotBlank(message = " 필수 입력 항목입니다.")
+  @Digits(integer = 3, fraction = 0, message = "CVV 입력 형식이 잘못되었습니다.")
   private String ccCvv;
-  
-  @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$",
-      message="만기년월 형식은 '월월/년년'입니다.")  
+
+  @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$", message = "만기년월 형식은 '월월/년년'입니다.")
   private String ccExprYM; // 만기년월
 
   public void addTaco(Taco taco) {

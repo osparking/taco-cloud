574cd113f373cdcebbea6c286c3c44e5481cca96
osparking <jbpark03@gmail.com>
2024-07-23 15:28:51 +0900

    맞춤 유저 서비스용 User 저장소 인터페이스

---
+++ src/main/java/tacos/data/UserRepository.java
@@ -0,0 +1,9 @@
+package tacos.data;
+
+import org.springframework.data.repository.CrudRepository;
+
+import tacos.entity.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+  User findByUsername(String username);
+}

ca5378b853f0ec596bf7df9becae5190dc7820b0
osparking <jbpark03@gmail.com>
2024-07-23 12:13:15 +0900

    메모리-내 두 사용자 kim, lee 등록 및 서비스 빈

--- src/main/java/tacos/config/SecuriConfi.java
+++ src/main/java/tacos/config/SecuriConfi.java
@@ -1,9 +1,18 @@
 package tacos.config;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecuriConfi {
@@ -11,5 +20,17 @@ public class SecuriConfi {
   @Bean
   PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  UserDetailsService userDetailsService(PasswordEncoder encoder) {
+    List<UserDetails> usersList = new ArrayList<>();
+    usersList.add(new User(
+        "kim", encoder.encode("1234"),
+        Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+    usersList.add(new User(
+        "lee", encoder.encode("1234"),
+        Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+    return new InMemoryUserDetailsManager(usersList);
   }
 }

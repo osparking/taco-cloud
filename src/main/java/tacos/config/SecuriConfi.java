4887d5e363a9a28d9c62ddef8859976871219621
osparking <jbpark03@gmail.com>
2024-07-24 19:15:37 +0900

    메모리-내 사용자 정적 등록 코드 제거

--- src/main/java/tacos/config/SecuriConfi.java
+++ src/main/java/tacos/config/SecuriConfi.java
@@ -1,20 +1,10 @@
 package tacos.config;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Formatter;
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,18 +13,6 @@ public class SecuriConfi {
   @Bean
   PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
-  }
-
-  @Bean
-  UserDetailsService userDetailsService(PasswordEncoder encoder) {
-    List<UserDetails> usersList = new ArrayList<>();
-    usersList.add(new User(
-        "kim", encoder.encode("1234"),
-        Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
-    usersList.add(new User(
-        "lee", encoder.encode("1234"),
-        Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
-    return new InMemoryUserDetailsManager(usersList);
   }
 
   @Bean

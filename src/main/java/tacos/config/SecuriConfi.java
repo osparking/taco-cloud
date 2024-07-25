3ef2f82617ec22158f355edf8e8f079443ca8a22
osparking <jbpark03@gmail.com>
2024-07-25 20:54:59 +0900

    h2-console 접근 때, 403 오류 해소

--- src/main/java/tacos/config/SecuriConfi.java
+++ src/main/java/tacos/config/SecuriConfi.java
@@ -3,6 +3,7 @@ package tacos.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -27,5 +28,10 @@ public class SecuriConfi {
         .defaultSuccessUrl("/design", true));
 
     return http.build();
+  }
+  
+  @Bean
+  WebSecurityCustomizer ignoringCustomizer() {
+    return (web) -> web.ignoring().requestMatchers("/h2-console/**");
   }
 }

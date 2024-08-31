66e64ed2ab7f48ea323cf5763de462a8a8618db3
osparking <jbpark03@gmail.com>
2024-08-31 22:23:55 +0900

    hand over logout message to login view

--- src/main/java/tacos/controller/LoginController.java
+++ src/main/java/tacos/controller/LoginController.java
@@ -1,14 +1,23 @@
 package tacos.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
   @GetMapping
-  public String loginForm() {
+  public String loginForm(Model model,
+      @RequestParam(required = false) String logout) {
+    String logoutMsg = null;
+
+    if (logout != null && "true".equals(logout)) {
+      logoutMsg = "성공적으로 로그아웃 되었습니다.";
+    }
+    model.addAttribute("logoutMsg", logoutMsg);
     return "login";
   }
 }

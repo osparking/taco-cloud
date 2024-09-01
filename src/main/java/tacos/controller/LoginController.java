31b52024f9ae5ee718c9d28f7eb905a3d1abd451
osparking <jbpark03@gmail.com>
2024-09-01 14:44:09 +0900

    use username adder in LoginController

--- src/main/java/tacos/controller/LoginController.java
+++ src/main/java/tacos/controller/LoginController.java
@@ -1,6 +1,5 @@
 package tacos.controller;
 
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import tacos.entity.TacoUser;
+import tacos.web.Utility;
 
 @Controller
 @RequestMapping("/login")
@@ -17,26 +16,14 @@ public class LoginController {
   public String loginForm(Model model,
       @RequestParam(required = false) String logout) {
     String logoutMsg = null;
-    var auth = SecurityContextHolder.getContext().getAuthentication();
 
     if (logout != null && "true".equals(logout)) {
-      auth.setAuthenticated(false);
+      SecurityContextHolder.getContext().getAuthentication().setAuthenticated(false);
       logoutMsg = "성공적으로 로그아웃 되었습니다.";
     }
     model.addAttribute("logoutMsg", logoutMsg);
-    
-    /**
-     * 유저 로그인 정보(username) 뷰단 전송
-     */
-    String username = null;
-
-    if (!(auth instanceof AnonymousAuthenticationToken)) {
-      username = ((TacoUser)auth.getPrincipal()).getUsername();
-    }
-    model.addAttribute("username", username);
-    
+    Utility.putUsernameTo(model);
+   
     return "login";
   }
 }
-
-

efd914fd26b85fae861d49d637518892c47bc64b
osparking <jbpark03@gmail.com>
2024-09-06 12:27:04 +0900

    check if /design accessed right before

--- src/main/java/tacos/controller/LoginController.java
+++ src/main/java/tacos/controller/LoginController.java
@@ -1,6 +1,10 @@
 package tacos.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.apache.catalina.authenticator.SavedRequest;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.savedrequest.DefaultSavedRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +17,20 @@ import tacos.web.Utility;
 @RequestMapping("/login")
 public class LoginController {
   @GetMapping
-  public String loginForm(Model model,
-      @RequestParam(required = false) String logout) {
+  public String loginForm(HttpServletRequest request, Model model,
+                          @RequestParam(required = false) String logout) {
     String logoutMsg = null;
 
+    HttpSession session = request.getSession();
+    var savedRequest = session.getAttribute("SPRING_SECURITY_SAVED_REQUEST");
+    if (savedRequest != null) {
+      var savedURL = ((DefaultSavedRequest) savedRequest).getRequestURL();
+      if (savedURL.equals("http://localhost:8080/design")) {
+        model.addAttribute("loginMsg", "타코 설계하려면, 로그인하세요 :-)");
+      } else {
+        model.addAttribute("loginMsg", null);
+      }
+    }
     if (logout != null && "true".equals(logout)) {
       SecurityContextHolder.getContext().getAuthentication().setAuthenticated(false);
       logoutMsg = "성공적으로 로그아웃 되었습니다.";

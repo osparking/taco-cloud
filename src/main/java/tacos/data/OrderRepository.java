b6bec9109d3a6b3da77b8d9d21b2ea66fab10ef8
osparking <jbpark03@gmail.com>
2024-09-11 17:04:25 +0900

    add repo method for a user's order list

--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -6,10 +6,13 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 import tacos.TacoOrder;
+import tacos.entity.TacoUser;
 
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
   List<TacoOrder> findByDeliZip(String deliZip);
 
   List<TacoOrder> readOrdersByDeliZipAndPlacedAtBetween(
       String deliveryZip, LocalDate startDate, LocalDate endDate);
+
+    List<TacoOrder> findByTacoUserOrderByPlacedAtDesc(TacoUser user);
 }

992c0e640c1ac287898d32546b3436bf07584b24
osparking <jbpark03@gmail.com>
2024-09-12 15:17:13 +0900

    make order reposi class method to read only one page

--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -3,6 +3,7 @@ package tacos.data;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import tacos.TacoOrder;
@@ -14,5 +15,6 @@ public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
   List<TacoOrder> readOrdersByDeliZipAndPlacedAtBetween(
       String deliveryZip, LocalDate startDate, LocalDate endDate);
 
-    List<TacoOrder> findByTacoUserOrderByPlacedAtDesc(TacoUser user);
+    List<TacoOrder> findByTacoUserOrderByPlacedAtDesc(TacoUser user, Pageable pageable);
 }
+

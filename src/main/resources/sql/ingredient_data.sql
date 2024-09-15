24c1b729cd4498f2b28828eb2e06599c47498159
osparking <jbpark03@gmail.com>
2024-09-15 16:43:04 +0900

    insert 10 ingredient records

---
+++ src/main/resources/sql/ingredient_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO tacocloud.ingredient (code,name,type_ord) VALUES
+	 ('FLTO','밀가루 토르티야',0),
+	 ('COTO','옥수수 토르티야',0),
+	 ('GRBF','다진 소고기',1),
+	 ('CARN','삶은 돼지고기',1),
+	 ('TMTO','토마토 깍뚜기',2),
+	 ('LETC','상추',2),
+	 ('CHED','체더치즈',3),
+	 ('JACK','몬테레이 잭',3),
+	 ('SLSA','살사',4),
+	 ('SRCR','사워 크림',4);

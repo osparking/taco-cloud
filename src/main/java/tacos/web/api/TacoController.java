a098745d2d5b7b19e22297fedf1f09a6190bc812
osparking <jbpark03@gmail.com>
2024-09-16 14:55:21 +0900

    define 'api/tacos?recent' request handler

--- src/main/java/tacos/web/api/TacoController.java
+++ src/main/java/tacos/web/api/TacoController.java
@@ -1,9 +1,13 @@
 package tacos.web.api;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tacos.Taco;
 import tacos.data.TacoRepository;
 
 @RestController
@@ -12,4 +16,11 @@ import tacos.data.TacoRepository;
 @AllArgsConstructor
 public class TacoController {
     private TacoRepository tacoRepository;
+
+    @GetMapping(params="recent")
+    public Iterable<Taco> recentTacos() {
+        PageRequest page = PageRequest.of(
+                0, 12, Sort.by("createdAt").descending());
+        return tacoRepository.findAll(page).getContent();
+    }
 }

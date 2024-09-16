aa527c59d01c0f212bb3528e70f267992e16233c
osparking <jbpark03@gmail.com>
2024-09-16 16:33:33 +0900

    name 10 ingredients to prepare taco creation

--- src/main/java/tacos/config/DataSourceBean.java
+++ src/main/java/tacos/config/DataSourceBean.java
@@ -36,16 +36,27 @@ public class DataSourceBean {
   @Profile("dev")
   ApplicationRunner ingredientLoader(IngredientRepository repo) {
     return (args) -> {
-      repo.save(new Ingredient("FLTO", "밀가루 토르티야", (short) 0));
-      repo.save(new Ingredient("COTO", "옥수수 토르티야", (short) 0));
-      repo.save(new Ingredient("GRBF", "다진 소고기", (short) 1));
-      repo.save(new Ingredient("CARN", "삶은 돼지고기", (short) 1));
-      repo.save(new Ingredient("TMTO", "토마토 깍뚜기", (short) 2));
-      repo.save(new Ingredient("LETC", "상추", (short) 2));
-      repo.save(new Ingredient("CHED", "체더치즈", (short) 3));
-      repo.save(new Ingredient("JACK", "몬테레이 잭", (short) 3));
-      repo.save(new Ingredient("SLSA", "살사", (short) 4));
-      repo.save(new Ingredient("SRCR", "사워 크림", (short) 4));
+      Ingredient ing1 = new Ingredient("FLTO", "밀가루 토르티야", (short) 0);
+      Ingredient ing2 = new Ingredient("COTO", "옥수수 토르티야", (short) 0);
+      Ingredient ing3 = new Ingredient("GRBF", "다진 소고기", (short) 1);
+      Ingredient ing4 = new Ingredient("CARN", "삶은 돼지고기", (short) 1);
+      Ingredient ing5 = new Ingredient("TMTO", "토마토 깍뚜기", (short) 2);
+      Ingredient ing6 = new Ingredient("LETC", "상추", (short) 2);
+      Ingredient ing7 = new Ingredient("CHED", "체더치즈", (short) 3);
+      Ingredient ing8 = new Ingredient("JACK", "몬테레이 잭", (short) 3);
+      Ingredient ing9 = new Ingredient("SLSA", "살사", (short) 4);
+      Ingredient ing10 = new Ingredient("SRCR", "사워 크림", (short) 4);
+
+      repo.save(ing1);
+      repo.save(ing2);
+      repo.save(ing3);
+      repo.save(ing4);
+      repo.save(ing5);
+      repo.save(ing6);
+      repo.save(ing7);
+      repo.save(ing8);
+      repo.save(ing9);
+      repo.save(ing10);
     };
   }
 }

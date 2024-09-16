69fa235fb3511985017f292269567ae06f22057a
osparking <jbpark03@gmail.com>
2024-09-16 17:17:55 +0900

    add three random tacos into h2 table

--- src/main/java/tacos/config/DataSourceBean.java
+++ src/main/java/tacos/config/DataSourceBean.java
@@ -10,7 +10,12 @@ import org.springframework.context.annotation.Configuration;
 
 import org.springframework.context.annotation.Profile;
 import tacos.Ingredient;
+import tacos.Taco;
 import tacos.data.IngredientRepository;
+import tacos.data.TacoCrudRepo;
+import tacos.data.TacoRepository;
+
+import java.util.Arrays;
 
 @Configuration
 public class DataSourceBean {
@@ -34,7 +39,8 @@ public class DataSourceBean {
 
   @Bean
   @Profile("dev")
-  ApplicationRunner ingredientLoader(IngredientRepository repo) {
+  ApplicationRunner ingredientLoader(IngredientRepository repo,
+                                     TacoCrudRepo tacoCrudRepo) {
     return (args) -> {
       Ingredient ing1 = new Ingredient("FLTO", "밀가루 토르티야", (short) 0);
       Ingredient ing2 = new Ingredient("COTO", "옥수수 토르티야", (short) 0);
@@ -57,6 +63,27 @@ public class DataSourceBean {
       repo.save(ing8);
       repo.save(ing9);
       repo.save(ing10);
+
+      Taco taco1 = new Taco();
+      taco1.setCheckedWrapId(ing1.getId());
+      taco1.setName("고기애호가");
+      taco1.setIncludedIngredients(Arrays.asList(
+              ing1, ing3, ing4, ing10, ing9, ing7));
+      tacoCrudRepo.save(taco1);
+
+      Taco taco2 = new Taco();
+      taco2.setCheckedWrapId(ing2.getId());
+      taco2.setName("단것이 좋아");
+      taco2.setIncludedIngredients(Arrays.asList(
+              ing2, ing7, ing8, ing10));
+      tacoCrudRepo.save(taco2);
+
+      Taco taco3 = new Taco();
+      taco3.setCheckedWrapId(ing2.getId());
+      taco3.setName("채식주의자");
+      taco3.setIncludedIngredients(Arrays.asList(
+              ing2, ing5, ing6, ing9));
+      tacoCrudRepo.save(taco3);
     };
   }
 }

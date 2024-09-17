0adb146cb53539569b2fca189dd793b1d5ec5931
osparking <jbpark03@gmail.com>
2024-09-17 10:01:40 +0900

    add taco creation request handler method

--- src/main/java/tacos/web/api/TacoController.java
+++ src/main/java/tacos/web/api/TacoController.java
@@ -38,4 +38,10 @@ public class TacoController {
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
     }
+
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco) {
+        return tacoCrudRepo.save(taco);
+    }
 }

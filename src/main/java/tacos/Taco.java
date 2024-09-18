8ca2556bb6c8418fa530512a43f150e0c8571c68
osparking <jbpark03@gmail.com>
2024-09-18 20:55:10 +0900

    assign relation and path name form taco entity

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -17,9 +17,11 @@ import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @Data
 @Entity
+@RestResource(rel="tacos", path="tacos")
 public class Taco {
 
   @Id

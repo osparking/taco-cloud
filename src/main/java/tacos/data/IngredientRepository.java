9a43493263b7024f251e9d1e5194978c8bcfdfbb
osparking <jbpark03@gmail.com>
2024-09-22 20:17:11 +0900

    get Ingredient info using 4 char' code - interface method

--- src/main/java/tacos/data/IngredientRepository.java
+++ src/main/java/tacos/data/IngredientRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.repository.CrudRepository;
 
 import tacos.Ingredient;
 
+import java.util.Optional;
+
 public interface IngredientRepository
     extends CrudRepository<Ingredient, Short> {
   Ingredient findByName(String name);
+  Optional<Ingredient> findByCode(String code);
 }

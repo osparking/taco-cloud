020759179f8b1ac064658b020dcef2ef2cbb1b1e
osparking <jbpark03@gmail.com>
2024-09-23 15:22:18 +0900

    enable preAuthorize at the main class level

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -2,9 +2,10 @@ package tacos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity(jsr250Enabled = true, securedEnabled = true)
 public class TacoCloudApplication {
 
 	public static void main(String[] args) {

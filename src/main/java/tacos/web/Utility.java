aa32d5b24f991493b9a4986db8487024dccbf1b2
osparking <jbpark03@gmail.com>
2024-09-26 15:39:11 +0900

    login user related code made comment

--- src/main/java/tacos/web/Utility.java
+++ src/main/java/tacos/web/Utility.java
@@ -2,14 +2,7 @@ package tacos.web;
 
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.ui.Model;
-
-import tacos.entity.TacoUser;
-
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 
 public class Utility {
   
@@ -23,25 +16,25 @@ public class Utility {
     String socialType = null;
 
     if (!(auth instanceof AnonymousAuthenticationToken)) {
-      if (auth.getPrincipal() instanceof OAuth2User) {
-        var token = (OAuth2AuthenticationToken) auth;
-        var attributes = ((OAuth2User) auth.getPrincipal()).getAttributes();
-
-        socialType = token.getAuthorizedClientRegistrationId();
-        switch (socialType) {
-          case "naver":
-            attributes = token.getPrincipal().getAttribute("response");
-            break;
-          case "kakao":
-            attributes = (token.getPrincipal().getAttribute("kakao_account"));
-            break;
-          default:
-            break;
-        }
-        username = attributes.get("email").toString();
-      } else {
-        username = ((TacoUser) auth.getPrincipal()).getUsername();
-      }
+//      if (auth.getPrincipal() instanceof OAuth2User) {
+//        var token = (OAuth2AuthenticationToken) auth;
+//        var attributes = ((OAuth2User) auth.getPrincipal()).getAttributes();
+//
+//        socialType = token.getAuthorizedClientRegistrationId();
+//        switch (socialType) {
+//          case "naver":
+//            attributes = token.getPrincipal().getAttribute("response");
+//            break;
+//          case "kakao":
+//            attributes = (token.getPrincipal().getAttribute("kakao_account"));
+//            break;
+//          default:
+//            break;
+//        }
+//        username = attributes.get("email").toString();
+//      } else {
+//        username = ((TacoUser) auth.getPrincipal()).getUsername();
+//      }
     }
     model.addAttribute("socialType", socialType);
     model.addAttribute("username", username);
